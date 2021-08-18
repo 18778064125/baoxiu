a37b85fbc0ff23aa32cb60222e47a986b1d819d8
littlelittlesix <md8817369>
2021-08-18 20:56:16 +0800

    好好做项目

---
+++ src/main/java/cn/edu/guet/baoxiu/BaoxiuApplication.java
@@ -0,0 +1,13 @@
+package cn.edu.guet.baoxiu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BaoxiuApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BaoxiuApplication.class, args);
+	}
+
+}
---
+++ src/test/java/cn/edu/guet/baoxiu/BaoxiuApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.edu.guet.baoxiu;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BaoxiuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
